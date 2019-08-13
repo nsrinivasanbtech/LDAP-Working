88ed6049cb4ba10507b0ccfcb9973cb9c1ae9d87
Pullagura Maruthi <Pullagura.Maruthi@bcbsma.com>
2019-08-13 14:36:19 +0530

    first commit

---
+++ src/main/java/com/bcbsma/LDAPApp/LdapAppApplication.java
@@ -0,0 +1,13 @@
+package com.bcbsma.LDAPApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class LdapAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LdapAppApplication.class, args);
+    }
+}
